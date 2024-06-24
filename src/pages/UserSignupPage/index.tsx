956d7aff12e9cf53d0b431fd260bcdea142c219b
chruscinskiDiego <contatodiegochruscinski@gmail.com>
2024-06-23 23:58:18 -0300

    feat: functional UserSignupPage

--- src/pages/UserSignupPage/index.tsx
+++ src/pages/UserSignupPage/index.tsx
@@ -1,78 +1,116 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import './style.css';
+import axios from 'axios';
+import logo from '../../assets/4clutch-logo.png';
 
 export function UserSignupPage() {
-  const [formData, setFormData] = useState({
-    nomeUsuario: '',
-    email: '',
-    senha: ''
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
     });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aqui você pode adicionar a lógica para enviar os dados para o servidor
-    console.log(formData);
-  };
+    const [errors, setErrors] = useState({
+        username: "",
+        email: "",
+        password: "",
+    });
 
-  return (
-    <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-      <Row>
-        <Col>
-          <div className="form-container">
-            <h1 className="text-center">Cadastro</h1>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group controlId="formNomeUsuario">
-                <Form.Label>Nome de Usuário</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="nomeUsuario"
-                  value={formData.nomeUsuario}
-                  onChange={handleChange}
-                  required
-                  className="form-input"
-                />
-              </Form.Group>
+    const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData((prev) => ({ ...prev, [name]: value }));
+        setErrors((prev) => ({ ...prev, [name]: "" })); // Limpa o erro do campo específico ao editar
+    };
 
-              <Form.Group controlId="formEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                  className="form-input"
-                />
-              </Form.Group>
+    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-              <Form.Group controlId="formSenha">
-                <Form.Label>Senha</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="senha"
-                  value={formData.senha}
-                  onChange={handleChange}
-                  required
-                  className="form-input"
-                />
-              </Form.Group>
+        const user = {
+            username: formData.username,
+            email: formData.email,
+            password: formData.password,
+        };
 
-              <Button variant="primary" type="submit" className="w-100 mt-3">
-                Cadastrar
-              </Button>
-            </Form>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  );
+        try {
+            const response = await axios.post('http://localhost:8025/users', user);
+            console.log("Usuário cadastrado com sucesso! - " + response.data.message);
+            // Resetar o formulário e erros após sucesso
+            setFormData({ username: "", email: "", password: "" });
+            setErrors({ username: "", email: "", password: "" });
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.validationErrors) {
+                const validationErrors = error.response.data.validationErrors;
+                setErrors((prev) => ({
+                    ...prev,
+                    ...validationErrors
+                }));
+            } else {
+                console.log("Falha ao cadastrar usuário - " + error.message);
+            }
+        }
+    };
+
+    return (
+        <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+            <Row>
+                <Col>
+                    <div className="form-container mx-auto" style={{ maxWidth: '500px' }}>
+                        <div className="d-flex justify-content-center align-items-center mb-3">
+                        <h1 className="" style={{ margin: 0 }}>Cadastro</h1>
+                        <img src={logo} alt="Logo" style={{ width: '100px', height: '100px', marginRight: '10px' }} />
+                        </div>
+                        <Form onSubmit={onSubmit}>
+                            <Form.Group controlId="formNomeUsuario">
+                                <Form.Label>Nome de Usuário</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    id="username"
+                                    name="username"
+                                    className={"form-input" + (errors.username ? " is-invalid" : "")}
+                                    onChange={onChange}
+                                    value={formData.username}
+                                    required
+                                />
+                                {errors.username && <div className="invalid-feedback">{errors.username}</div>}
+                            </Form.Group>
+
+                            <Form.Group controlId="formEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    className={"form-input" + (errors.email ? " is-invalid" : "")}
+                                    onChange={onChange}
+                                    value={formData.email}
+                                    required
+                                />
+                                {errors.email && <div className="invalid-feedback">{errors.email}</div>}
+                            </Form.Group>
+
+                            <Form.Group controlId="formSenha">
+                                <Form.Label>Senha</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    className={"form-input" + (errors.password ? " is-invalid" : "")}
+                                    onChange={onChange}
+                                    value={formData.password}
+                                    required
+                                />
+                                {errors.password && <div className="invalid-feedback">{errors.password}</div>}
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit" className="w-100 mt-3 form-button">
+                                Cadastrar
+                            </Button>
+                        </Form>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
