1bc850802d69f79466d64d1cee1688d5f6793ced
GustavoMoretto <gustavomorettodalla1@gmail.com>
2024-07-02 02:42:36 -0300

    feat: footer component

--- src/routes/AuthenticatedRoutes/index.tsx
+++ src/routes/AuthenticatedRoutes/index.tsx
@@ -1,23 +1,22 @@
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import AuthService from "../../services/AuthService"
-import { Navbar } from "../../components/Navbar";
+import React from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import AuthService from '../../services/AuthService';
+import { Navbar } from '../../components/Navbar';
+import { Footer } from '../../components/Footer'; // Importe o Footer aqui
 
 export function AuthenticatedRoutes() {
-
     const isAuthenticated = AuthService.isAuthenticated();
     const location = useLocation();
 
-    return(
-        isAuthenticated ? (
+    return isAuthenticated ? (
         <>
-            <Navbar/>
-            {/* Coringa para exibir o elemento na BaseRoutes*/}
-            <Outlet/>
-        
-        </>) : (
-            <>
-                <Navigate to="/login" state={{from:location}} replace />
-            </>
-        )
-    )
+            <Navbar />
+            <Outlet />
+            <Footer /> {/* Adicione o Footer aqui */}
+        </>
+    ) : (
+        <>
+            <Navigate to="/login" state={{ from: location }} replace />
+        </>
+    );
 }
